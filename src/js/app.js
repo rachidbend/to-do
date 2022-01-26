7ddbc494c0aad07ca87521eb0616f61dcfba3a1a
rachidbend <rachidbendaghor@gmail.com>
2022-01-26 14:10:59 +0100

    added html markup, adjustments may be needed

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 console.log('hi there to-do');
+
+// var el = document.getElementById('items');
+// var sortable = Sortable.create(el, {
+//   sort: true,
+// });
+
+// console.log(el);
+// console.log(sortable);
