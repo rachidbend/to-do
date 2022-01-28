05bcfd9bb438c72f92398bbfeb2ae4a6eed782ba
rachidbend <rachidbendaghor@gmail.com>
2022-01-28 23:12:05 +0100

    added further styling to the items

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 console.log('hi there to-do');
 
-// var el = document.getElementById('items');
-// var sortable = Sortable.create(el, {
-//   sort: true,
-// });
+var el = document.getElementById('items');
+var sortable = Sortable.create(el, {
+  sort: true,
+});
 
-// console.log(el);
-// console.log(sortable);
+console.log(el);
+console.log('helloooooooooooooooo');
